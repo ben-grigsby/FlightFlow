520afe961f417d42d6d99766efdb6ba3e7ef4f04
Ben Grigsby <northern0207@gmail.com>
2025-10-30 10:41:03 +0800

    Update .gitignore

--- dashboard/configs/configs.py
+++
@@ -1,8 +0,0 @@
-# dashboard/configs/configs.py
-
-timezones = {
-    "New York": "America/New_York",
-    "London": "Europe/London",
-    "Tokyo": "Asia/Tokyo",
-    "Los Angeles": "America/Los_Angeles"
-}
